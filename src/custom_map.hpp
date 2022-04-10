5efcd39b7b87cd2f77c2ebef863f8c6675bfa0bd
AzethM <meronentertainment@gmail.com>
2022-04-11 00:10:34 +0200

    Final change, now it should be fast&safe

--- src/custom_map.hpp
+++ src/custom_map.hpp
@@ -7,16 +7,12 @@ namespace Graphs
 	template<typename keytype, typename valuetype>
 	class iterator : public std::unordered_map<keytype, valuetype>::iterator
 	{
-		private:
-			typename std::unordered_map<keytype, valuetype>::iterator* cast_to_base()
-			{
-				return ((typename std::unordered_map<keytype, valuetype>::iterator*) this);
-			}
 		public:
 			valuetype* operator -> () = delete;
 			valuetype& operator * ()
 			{
-				return (*cast_to_base())->second;
+				typename std::unordered_map<keytype, valuetype>::iterator& iter = *this;
+				return iter->second;
 			}
 			iterator<keytype, valuetype>(const typename std::unordered_map<keytype, valuetype>::iterator& i) : std::unordered_map<keytype, valuetype>::iterator(i) {}
 	};
@@ -24,16 +20,12 @@ namespace Graphs
 	template<typename keytype, typename valuetype>
 	class const_iterator : public std::unordered_map<keytype, valuetype>::const_iterator
 	{
-		private:
-			typename std::unordered_map<keytype, valuetype>::const_iterator* cast_to_base() const
-			{
-				return ((typename std::unordered_map<keytype, valuetype>::const_iterator*) this);
-			}
 		public:
 			const valuetype* operator -> () const = delete;
 			const valuetype& operator * () const
 			{
-				return (*cast_to_base())->second;
+				const typename std::unordered_map<keytype, valuetype>::const_iterator& iter = *this;
+				return iter->second;
 			}
 			const_iterator<keytype, valuetype>(const typename std::unordered_map<keytype, valuetype>::const_iterator& i) : std::unordered_map<keytype, valuetype>::const_iterator(i) {}
 	};
