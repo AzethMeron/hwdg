ea91f54773ffda8bd852bc6b68aefa5ac80d09e2
AzethMeron <meronentertainment@gmail.com>
2022-04-16 16:40:24 +0200

    Bellman-Ford Algorithm finished

--- src/bellmanford.cpp
+++ src/bellmanford.cpp
@@ -25,6 +25,32 @@ namespace Graphs
 		{
 			this->_results.insert({ node.id(), PathtableCell(node, this->_source) });
 		}
+		// Algorithm
+		for (int i = 0; i < graph.size_nodes() - 1; ++i)
+		{
+			bool has_changed = false;
+			for (const Edge& edge : graph.edges())
+			{
+				const PathtableCell& src = this->getCell(edge.source());
+				const PathtableCell& tgt = this->getCell(edge.target());
+				if (tgt.pathweight > src.pathweight + edge.weight())
+				{
+					this->UpdateWeight(tgt.node, src.node, src.pathweight + edge.weight());
+					has_changed = true;
+				}
+			}
+			if (!has_changed) break;
+		}
+		// Check for negative cycle
+		for (const Edge& edge : graph.edges())
+		{
+			const PathtableCell& src = this->getCell(edge.source());
+			const PathtableCell& tgt = this->getCell(edge.target());
+			if (tgt.pathweight > src.pathweight + edge.weight())
+			{
+				throw std::invalid_argument("Negative cycle detected");
+			}
+		}
 	}
 
 	const unordered_map<uint32_t, PathtableCell>& BellmanFord::RawResults(void) const
@@ -48,5 +74,24 @@ namespace Graphs
 		PathtableCell& c = (*this->_results.find(node.id()));
 		c.pathweight = pathweight;
 		c.prev_id = prev_node.id();
+	}
+
+	Path BellmanFord::GetPath(const Node& target) const
+	{
+		if (!this->has(target)) throw std::invalid_argument(string_format("No node %s in graph this algorithm was used on", target.str()));
+		std::vector<Node> nodes;
+		const double weight = this->getCell(target).pathweight;
+		const bool exists = this->getCell(target).prev_id != -1;
+		Node analysing = target;
+		nodes.push_back(analysing);
+		while (true)
+		{
+			const PathtableCell& c = this->getCell(analysing);
+			if (c.prev_id < 0) break;
+			analysing = Node(c.prev_id);
+			nodes.push_back(analysing);
+		}
+		std::reverse(nodes.begin(), nodes.end());
+		return Path(nodes, weight, exists);
 	}
 }
