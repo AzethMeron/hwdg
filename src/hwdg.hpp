68c63884120a70aee05e91b7df710a0a3a02fde4
AzethM <meronentertainment@gmail.com>
2022-05-25 17:54:19 +0200

    HWDG header

---
+++ src/hwdg.hpp
@@ -0,0 +1,14 @@
+#ifndef HWDG_HWDG_HPP
+#define HWDG_HWDG_HPP
+
+#include "node.hpp"
+#include "edge.hpp"
+#include "node_in_graph.hpp"
+#include "graph.hpp"
+#include "tools.hpp"
+#include "operations.hpp"
+#include "path.hpp"
+#include "bellmanford.hpp"
+#include "dijkstra.hpp"
+
+#endif
